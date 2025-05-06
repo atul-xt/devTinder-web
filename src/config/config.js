0f8b238139205779258b3800ca26b77a04be68d3
atul-xt <atul.xtt@gmail.com>
2025-05-06 22:25:46 +0530

    config url changed

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,1 @@
-// export const API_URL = "http://localhost:3000/";
-export const API_URL = "/api/";
-// export const API_URL = "https://devtinder-dwwk.onrender.com/";
+export const API_URL = location.hostname === "localhost" ? "http://localhost:3000/" : "/api/";
